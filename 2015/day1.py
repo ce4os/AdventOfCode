157be1e43084e8b889f0a3e1d87f96097a089249
c4os <ce4os@proton.me>
2024-03-19 22:01:12 +0100

    Added 2015/day3, fixed typo in day1

--- 2015/day1.py
+++ 2015/day1.py
@@ -4,13 +4,13 @@ directions = get_puzzle_input_as_string("src/day1_input")
 
 # Part 1 - Santa delivers presents in a large appartement building
 def process_instructions(directions: str):
-    floor = sum([1 if char == "(" else -1 for char in instructions])
+    floor = sum([1 if char == "(" else -1 for char in directions])
     return floor
 
 # Part 2 - Santa in the basement
 def process_instructions2(directions: str):
     floor = 0
-    for position, char in enumerate(instructions):
+    for position, char in enumerate(directions):
         if char == "(": 
             floor += 1
         else:
---
+++ 2015/day3.py
@@ -0,0 +1,41 @@
+from utilities import get_puzzle_input_as_string
+
+moves = get_puzzle_input_as_string("src/day3_input")
+
+# Part 1 - Instructions from an egnogged elf
+
+def get_visitied_houses(moves: str):
+    coordinates = [0,0]
+    visited_houses = [(0,0)]
+    for move in moves:
+        if move == ">":
+            coordinates[0] += 1
+        elif move == "<":
+            coordinates[0] -= 1
+        elif move == "^":
+            coordinates[1] += 1
+        else:
+            coordinates[1] -= 1    
+        visited_houses.append(tuple(coordinates))
+    return visited_houses
+
+houses_visited_by_santa = get_visitied_houses(moves)
+number_of_houses_visited = len(set(houses_visited_by_santa))
+
+# Part 2 - With a little help from Robosanta
+
+moves_for_santa = ""
+moves_for_robosanta = ""
+for counter, char in enumerate(moves):
+    if counter%2 == 0:
+        moves_for_robosanta += char
+    else:
+        moves_for_santa += char
+
+houses_visited = get_visitied_houses(moves_for_santa)
+houses_visited.extend(get_visitied_houses(moves_for_robosanta))
+number_of_houses_visited2 = len(set(houses_visited))
+
+# Results
+print(number_of_houses_visited)
+print(number_of_houses_visited2)
