09955005187506cf599e08d300f8976c1234e381
c4os <ce4os@proton.me>
2024-06-01 16:33:38 +0200

    Update 2015/day1.py; Add 2015/day2.py 2015/src/day2_input

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,15 +1,17 @@
 # --- Day 1: Not Quite Lisp ---
 
-# Imports
 from utils import get_puzzle_input_as_string
 
-# Get Puzzle input
+
+# Get puzzle input
 directions = get_puzzle_input_as_string("src/day1_input")
+
 
 # Part 1 - Santa delivers presents in a large appartement building
 def process_instructions(directions: str) -> int:
     floor = sum([1 if char == "(" else -1 for char in directions])
     return floor
+
 
 # Part 2 - Santa in the basement
 def process_instructions2(directions: str) -> int:
@@ -21,6 +23,7 @@ def process_instructions2(directions: str) -> int:
             floor -= 1
         if floor == -1:
             return position + 1
+
 
 # Results
 print("Result Part1: ", process_instructions(directions))
---
+++ 2015/day2.py
@@ -0,0 +1,31 @@
+# --- Day 2: I Was Told There Would Be No Math ---
+
+from utils import get_puzzle_input_as_list
+
+
+# Get puzzle input
+package_dimensions = get_puzzle_input_as_list("src/day2_input")
+
+# Initialize variables
+total_wrapping_paper = 0
+total_ribbon = 0
+
+
+# compute results
+for package_dimension in package_dimensions:
+    # Part 1 - Wrap the box
+    length, width, height = [int(side_length) for side_length in package_dimension.split("x")]
+    surface_of_box = 2*(length*width + width*height + length * height)
+    area_of_smallest_side = min([length * width, width * height, height * length])
+    total_wrapping_paper += surface_of_box + area_of_smallest_side
+
+    # Part 2 - Add ribbons
+    bow = length * width * height
+    package_dimension = [length, width, height]
+    package_dimension.remove(max(package_dimension))
+    ribbon = 2*sum(package_dimension)
+    total_ribbon += bow + ribbon
+    
+    
+print("Result Part1: ", total_wrapping_paper)
+print("Result Part2: ", total_ribbon)
